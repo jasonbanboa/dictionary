6182a02a56ee90403ec878c7c4d2f5eaa08f5b85
jasonbanboa <jasonbanboa123@gmail.com>
2023-04-20 16:00:25 +0900

    added theme toggle button

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,38 @@
 import './style.css'
-import { WordData, ErrorWord } from './dictionaryService'
+// import { WordData, ErrorWord } from './dictionaryService'
 import fetchWordData from './dictionaryService'
+
+
+const $toggleButton = document.querySelector<HTMLDivElement>('.toggle-button')!;
+
+
+const events = {
+  onThemeButtonClick: () => {
+    if ($toggleButton.classList.contains('light-theme')) {
+      document.body.classList.replace('light-theme', 'dark-theme');
+      $toggleButton.classList.replace('light-theme', 'dark-theme');
+    } else {
+      document.body.classList.replace('dark-theme', 'light-theme');
+      $toggleButton.classList.replace('dark-theme', 'light-theme');
+    }
+  }
+} 
+
 
 async function main() {
   const responseData = await fetchWordData('hello');
 
   if (responseData instanceof Array)  {
     const [ WORDDATA ] = responseData;
-    console.log(WORDDATA); 
-
-    
-
-
+    console.log(WORDDATA);     
   } else {
-    const WORDERROR: ErrorWord = responseData;
-    console.log(WORDERROR)
+    const WORDERROR = responseData;
+    console.log(WORDERROR);
   }
 }
 
+
 window.onload = () => {
   main();
+  $toggleButton.addEventListener('click', events.onThemeButtonClick);
 }
