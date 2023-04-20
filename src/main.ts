ec3b16fe792dbc95d1a5c8dbd236829260871564
jasonbanboa <jasonbanboa123@gmail.com>
2023-04-20 23:56:58 +0900

    feat search for words and added style

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,10 @@ import fetchWordData from './dictionaryService'
 
 
 const $toggleButton = document.querySelector<HTMLDivElement>('.toggle-button')!;
+const $form = document.querySelector<HTMLFormElement>('form')!;
 
 
-const events = {
+const handlers = {
   onThemeButtonClick: () => {
     if ($toggleButton.classList.contains('light-theme')) {
       document.body.classList.replace('light-theme', 'dark-theme');
@@ -15,24 +16,34 @@ const events = {
       document.body.classList.replace('dark-theme', 'light-theme');
       $toggleButton.classList.replace('dark-theme', 'light-theme');
     }
+  },
+  onSearchButtonClick: () => {
+    $form.submit();
+  }, 
+  onFormSubmit: async (e: Event) => {
+    e.preventDefault();
+    const searchWord = $form.search.value.trim();
+    if (!searchWord) return;
+    const responseData = await fetchWordData(searchWord);
+    if (responseData instanceof Array)  {
+      const [ WORDDATA ] = responseData;
+      console.log(WORDDATA);     
+    } else {
+      const WORDERROR = responseData;
+      console.log(WORDERROR);
+    }
   }
 } 
 
 
 async function main() {
-  const responseData = await fetchWordData('hello');
-
-  if (responseData instanceof Array)  {
-    const [ WORDDATA ] = responseData;
-    console.log(WORDDATA);     
-  } else {
-    const WORDERROR = responseData;
-    console.log(WORDERROR);
-  }
+  // const responseData = await fetchWordData('hello');
+  
 }
 
 
 window.onload = () => {
   main();
-  $toggleButton.addEventListener('click', events.onThemeButtonClick);
+  $toggleButton.addEventListener('click', handlers.onThemeButtonClick);
+  $form.addEventListener('submit', handlers.onFormSubmit);
 }
