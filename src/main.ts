96d400c2e0a8695102e5a7f464e77edd7f4125d6
jasonbanboa <jasonbanboa123@gmail.com>
2023-04-22 11:38:55 +0900

    feat font selection

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,26 @@ import './style.css'
 import fetchWordData, { WordData } from './dictionaryService'
 import { renderError, renderWord } from './renderService';
 
-
+const $app = document.querySelector<HTMLDivElement>('#app')!;
 const $toggleButton = document.querySelector<HTMLDivElement>('.toggle-button')!;
 const $form = document.querySelector<HTMLFormElement>('form')!;
 const $fontSelector = document.querySelector<HTMLDivElement>('.font-selector')!;
 const $fontSelect = document.querySelector<HTMLDivElement>('.font-select')!;
+const $fonts = $fontSelect.querySelectorAll('.font')!;
+const $currentFont = document.querySelector<HTMLDivElement>('#current-font')!;
 
+$fonts.forEach(($font, i) => {
+  $font.addEventListener('click', (e: Event) => {
+    const $fontDiv = e.target;
+    if ($fontDiv instanceof HTMLDivElement) {
+      const { font } = $fontDiv.dataset;
+      $fonts.forEach($font => $font.classList.remove('primary'));
+      $fonts[i].classList.add('primary');
+      $app.className = font!; 
+      $currentFont.textContent = font!;
+    }
+  })
+});
 
 const handlers = {
   onThemeButtonClick: () => {
@@ -40,7 +54,7 @@ const handlers = {
   },
   onFontThemeClick: (e: Event) => {
     if (e.target === $fontSelect) return;
-    $fontSelect.classList.contains('none') ? $fontSelect.classList.replace('none', 'block') : $fontSelect.classList.replace('block', 'none');
+    $fontSelect.classList.contains('none') ? $fontSelect.classList.replace('none', 'flex') : $fontSelect.classList.replace('flex', 'none');
   }
 } 
 
